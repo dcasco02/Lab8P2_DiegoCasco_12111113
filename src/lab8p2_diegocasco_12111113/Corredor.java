ea49c68d2854d0ef8f9b46059fa13c4475e96f10
dcasco02 <dcasco@unitec.edu>
2022-03-11 14:40:27 -0600

    creacion archivo bin

--- src/lab8p2_diegocasco_12111113/Corredor.java
+++ src/lab8p2_diegocasco_12111113/Corredor.java
@@ -10,5 +10,65 @@ package lab8p2_diegocasco_12111113;
  * @author dcasc
  */
 public class Corredor {
+    private String nombreCorredor;
+    private int Identificador;
+    private String tipoauto;
+    private String Color;
+    private int distanciarecorrida;
     
+    public Corredor() {
+    }
+
+    public Corredor(String nombreCorredor, int Identificador, String tipoauto, String Color, int distanciarecorrida) {
+        this.nombreCorredor = nombreCorredor;
+        this.Identificador = Identificador;
+        this.tipoauto = tipoauto;
+        this.Color = Color;
+        this.distanciarecorrida = 0;
+    }
+
+    public String getNombreCorredor() {
+        return nombreCorredor;
+    }
+
+    public void setNombreCorredor(String nombreCorredor) {
+        this.nombreCorredor = nombreCorredor;
+    }
+
+    public int getIdentificador() {
+        return Identificador;
+    }
+
+    public void setIdentificador(int Identificador) {
+        this.Identificador = Identificador;
+    }
+
+    public String getTipoauto() {
+        return tipoauto;
+    }
+
+    public void setTipoauto(String tipoauto) {
+        this.tipoauto = tipoauto;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public int getDistanciarecorrida() {
+        return distanciarecorrida;
+    }
+
+    public void setDistanciarecorrida(int distanciarecorrida) {
+        this.distanciarecorrida = distanciarecorrida;
+    }
+
+    @Override
+    public String toString() {
+        return  nombreCorredor;
+    }
 }
--- src/lab8p2_diegocasco_12111113/listacorredores.java
+++ src/lab8p2_diegocasco_12111113/listacorredores.java
@@ -5,10 +5,88 @@
  */
 package lab8p2_diegocasco_12111113;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 /**
  *
  * @author dcasc
  */
 public class listacorredores {
+    private ArrayList <Corredor> listacorredores=new ArrayList();
+    private File Corredores = null;
+
+    private listacorredores (String path){
+        Corredores=new File(path);
+    }
+    
+    public ArrayList<Corredor> getListacorredores() {
+        return listacorredores;
+    }
+
+    public void setListacorredores(ArrayList<Corredor> listacorredores) {
+        this.listacorredores = listacorredores;
+    }
+
+    public File getCorredores() {
+        return Corredores;
+    }
+
+    public void setCorredores(File Corredores) {
+        this.Corredores = Corredores;
+    }
+
+    @Override
+    public String toString() {
+        return "listacorredores=" + listacorredores;
+    }
+    public void cargarArchivo() {
+        try {            
+            listacorredores = new ArrayList();
+            Corredor temp;
+            if (Corredores.exists()) {
+                FileInputStream entrada
+                    = new FileInputStream(Corredores);
+                ObjectInputStream objeto
+                    = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Corredor) objeto.readObject()) != null) {
+                        listacorredores.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            }            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(Corredores);
+            bw = new ObjectOutputStream(fw);
+            for (Corredor t : listacorredores) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
     
 }
